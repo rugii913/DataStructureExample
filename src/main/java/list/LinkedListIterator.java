164c4170d169a9b86969462b6ada6ca6518640c8
rugii913 <rugii913@gmail.com>
2023-07-03 11:31:22 +0900

    p.164 iterator 인터페이스 - 구현 2

--- src/main/java/list/LinkedListIterator.java
+++ src/main/java/list/LinkedListIterator.java
@@ -4,6 +4,8 @@ import java.util.Iterator;
 
 public class LinkedListIterator implements Iterator<Node> {
 
+    // 구현 1
+    /*
     private Node current;
 
     public LinkedListIterator(LinkedList list) { // java.util의 LinkedList가 아니라 앞에서 만든 LinkedList임
@@ -18,5 +20,25 @@ public class LinkedListIterator implements Iterator<Node> {
     @Override //implements Iterator - remove()와 forEachRemaining()은 default로 재정의하지 않아도 됨
     public Node next() {
         return current = current.next;
+    }
+    */
+
+    // 구현 2
+    private Node nextNode;
+
+    public LinkedListIterator(LinkedList list) {
+        nextNode = list.getNode(0); // 0th Node
+    }
+
+    @Override
+    public boolean hasNext() {
+        return nextNode != null;
+    }
+
+    @Override
+    public Node next() { // 필드 nextNode에는 nextNode.next를 저장하고, 원래 nextNode였던 것을 반환
+        Node tmp = nextNode;
+        nextNode = nextNode.next;
+        return tmp;
     }
 }
--- src/test/java/list/LinkedListIteratorTest.java
+++ src/test/java/list/LinkedListIteratorTest.java
@@ -18,7 +18,15 @@ class LinkedListIteratorTest {
         LinkedListIterator iter = new LinkedListIterator(list);
         while (iter.hasNext()) {
             System.out.println(iter.next().item);
-            // 구현 1: 생성 후 처음 iter.next()하면 index 0 노드를 반환
+            // 구현 1: 생성 후 current는 더미 헤드
+            // 처음 iter.next()하면
+            // index 0 노드를 current에 할당하면서, 새로 할당된 current(= index 0 노드)를 반환
+            // 출력 결과: 100 200 500 600 700
+
+            // 구현 2: 생성 후 nextNode는 index 0 노드
+            // 처음 iter.next()하면
+            // 원래의 nextNode는 tmp로 참조하고 
+            // index 1 노드를 nextNode에 할당하면서, tmp로 참조한 index 0 노드를 반환 
             // 출력 결과: 100 200 500 600 700
         }
     }
