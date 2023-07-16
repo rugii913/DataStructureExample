33b733dc459ab92e2a38dd65557940fc52cad442
rugii913 <rugii913@gmail.com>
2023-07-16 15:33:32 +0900

    <Queue> p.234 InheritedQueue ext LinkedList impl QueueInterface
    * 상속을 통한 소프트웨어 재사용의 예
    ** 원형으로 해석된 것 아님

---
+++ src/main/java/queue/InheritedQueue.java
@@ -0,0 +1,30 @@
+package queue;
+
+import list.LinkedList;
+
+public class InheritedQueue<E> extends LinkedList<E> implements QueueInterface<E> {
+
+    public InheritedQueue() {
+        super();
+    }
+
+    @Override
+    public void enqueue(E newItem) {
+        append(newItem);
+    }
+
+    @Override
+    public E dequeue() {
+        return remove(0);
+    }
+
+    @Override
+    public E front() {
+        return get(0);
+    }
+
+    @Override
+    public void dequeueAll() {
+        clear();
+    }
+}
