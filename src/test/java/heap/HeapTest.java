29a152c05570d269ad6d009a86e3e9d69fe89034
rugii913 <rugii913@gmail.com>
2023-07-17 16:43:19 +0900

    <Heap> Heap insert() overflow 예외 테스트

--- src/test/java/heap/HeapTest.java
+++ src/test/java/heap/HeapTest.java
@@ -11,9 +11,22 @@ class HeapTest {
     void clearTest() throws PQException {
         Heap<Integer> heap = new Heap<>(5);
         heap.insert(1);
-        heap.insert(1);
+        heap.insert(10);
         heap.clear();
         Assertions.assertEquals(true, heap.isEmpty());
+    }
+
+    @Test
+    void exceptionTest() throws PQException {
+        Heap<Integer> heap = new Heap<>(5);
+        heap.insert(10);
+        heap.insert(20);
+        heap.insert(30);
+        heap.insert(40);
+        heap.insert(50);
+        heap.deleteMax();
+        heap.insert(1);
+        Assertions.assertThrows(PQException.class, () -> heap.insert(2), "HeapErr: Insert()-Overflow!");
     }
 
 }
